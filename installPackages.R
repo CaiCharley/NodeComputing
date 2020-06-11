777231f2e472602a3a45d59f6a844c12a4cf6c3c
CaiCharley <charley.cai113@gmail.com>
2020-06-10 20:13:11 -0700

    add installpackages.R

---
+++ installPackages.R
@@ -0,0 +1,26 @@
+# installs a list of packages
+currentpkgs = installed.packages()[,1] 
+
+# required packages
+pkgnames = c(
+  "tidyverse",
+  "BiocManager",
+  "magrittr",
+  "argparse",
+  "readxl"
+)
+
+missingpkgs = setdiff(pkgnames, currentpkgs)
+
+# install package
+(R_INSTALL_STAGED = FALSE)
+install.packages(missingpkgs, dependencies = T, quiet = T)
+currentpkgs = installed.packages()[,1]
+missingpkgs = setdiff(pkgnames, currentpkgs)
+
+if (!length(missingpkgs) == 0) {
+  noquote(c("The following packages(s) failed to install: ", missingpkgs))
+}
+
+# proprietary installation
+BiocManager::install("PrInCE")
