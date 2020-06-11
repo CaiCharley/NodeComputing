74e3e1dcb52af38284548320d915c15022658b72
CaiCharley <charley.cai113@gmail.com>
2020-06-10 21:11:40 -0700

    fix order and staged install in installpackages.r

--- installPackages.R
+++ installPackages.R
@@ -1,5 +1,5 @@
 # installs a list of packages
-currentpkgs = installed.packages()[,1] 
+currentpkgs = installed.packages()[, 1]
 
 # required packages
 pkgnames = c(
@@ -13,16 +13,19 @@ pkgnames = c(
 missingpkgs = setdiff(pkgnames, currentpkgs)
 
 # install package
-(R_INSTALL_STAGED = FALSE)
+Sys.setenv(R_INSTALL_STAGED = FALSE)
 install.packages(missingpkgs, dependencies = T, quiet = T)
-currentpkgs = installed.packages()[,1]
+currentpkgs = installed.packages()[, 1]
 missingpkgs = setdiff(pkgnames, currentpkgs)
+
+# proprietary installation
+BiocManager::install("PrInCE")
+if (!requireNamespace("PrInCE", quietly = TRUE))
+  BiocManager::install("PrInCE")
+# BiocManager requires nc-config, get with "sudo apt-get install r-cran-ncdf4"  
 
 if (!length(missingpkgs) == 0) {
   noquote(c("The following packages(s) failed to install: ", missingpkgs))
 } else {
   noquote("All packages installed successfully")
 }
-
-# proprietary installation
-BiocManager::install("PrInCE")
