a153f43b620cd3f1b1549775b5b9d34a17554805
CaiCharley <charley.cai113@gmail.com>
2020-06-13 21:45:36 -0700

    add module loading for CC

--- bench-prince.sh
+++ bench-prince.sh
@@ -3,6 +3,14 @@
 
 cd ~/OneDrive/git/PrinceR
 
+# load R if on Compute Canada
+OS=$(lsb_release -is)
+if [ "$OS" == "CentOS" ]; then
+    module load nixpkgs/16.09  
+    module load gcc/7.3.0
+    module load r/4.0.0
+fi
+
 # R Script Location
 RSCRIPTPATH=~/OneDrive/git/PrinceR/inner-prince.R
 
