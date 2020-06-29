b4679a62125079e79a538b93113b118c33b4112f
CaiCharley <charley.cai113@gmail.com>
2020-06-29 15:09:15 -0700

    moved to dedicated folder

--- princeR/graphscripts/analyze_benchmarks.R
+++ princeR/graphscripts/analyze_benchmarks.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 
-benchmarks <- readRDS("/mnt/d/Downloads/benchmarks.rds") 
+benchmarks <- readRDS("/mnt/d/Downloads/benchmarks.rds")
 names(benchmarks)[1] <- "filepath"
 
 benchmarks %<>% mutate(
@@ -10,9 +10,11 @@ benchmarks %<>% mutate(
   classifier = str_extract(filepath, "(?<=classifier=).*(?=-)"),
   dataset = (sub("-.*", "", filepath))
 )
-col_order <- c("filepath", "dataset", "classifier", "nmodels",
-  "Peak_RAM_Used_MiB", "min", "lq", "mean", "median", "uq" ,"max", "neval")
-benchmarks <- benchmarks[, col_order] 
+col_order <- c(
+  "filepath", "dataset", "classifier", "nmodels",
+  "Peak_RAM_Used_MiB", "min", "lq", "mean", "median", "uq", "max", "neval"
+)
+benchmarks <- benchmarks[, col_order]
 
 group_by(benchmarks, dataset, nmodels, classifier)
 
@@ -24,12 +26,12 @@ ram <- ggplot(benchmarks) +
 
 ram_facet <- ggplot(benchmarks) +
   geom_boxplot(aes(Peak_RAM_Used_MiB, reorder(classifier, Peak_RAM_Used_MiB))) +
-  facet_grid(nmodels~dataset)
+  facet_grid(nmodels ~ dataset)
 
 time_facet <- ggplot(benchmarks) +
   geom_boxplot(aes(mean, reorder(classifier, median))) +
   coord_cartesian(xlim = c(0, 5000)) +
-  facet_grid(nmodels~dataset)
+  facet_grid(nmodels ~ dataset)
 
 ggsave("/mnt/d/Downloads/bench_ram.png", ram, device = "png")
 ggsave("/mnt/d/Downloads/bench_timefacet.png", time_facet, device = "png")
