8fe751f4126e30cc57f69e596b1e11266b38f1c6
CaiCharley <charley.cai113@gmail.com>
2020-07-13 00:41:14 -0700

    make heatmap

--- princeR/detect_complexes/scott_detect_complexes.R
+++ princeR/detect_complexes/scott_detect_complexes.R
@@ -1,15 +1,32 @@
 # generate heatmap with detect_complexes on scott dataset
 setwd("/mnt/d/Downloads/scottdata")
 
+library(tidyverse)
+library(magrittr)
+library(heatmap3)
+
 # load data as individual list element
 goldstd <- readRDS("goldstd.rds")
 file_names <- list.files(pattern = "[^goldstd.rds]")
-files <- map(file_names, ~ readRDS(.)) %>% 
-  setNames(str_replace(file_names, ".rds", "")) %>% 
+files <- map(file_names, ~ readRDS(.)) %>%
+  setNames(str_replace(file_names, ".rds", "")) %>%
   unlist(recursive = F)
 
 # detect complexes
 goldcmplx <- goldstd[lengths(goldstd) > 2]
 
-scott_complexes <- map(files, ~ detect_complexes(., goldcmplx) %>% 
-    na.omit())
+scott_complexes <- map(files, ~ detect_complexes(., goldcmplx) %>%
+  na.omit() %>%
+  as_tibble(rownames = "complex"))
+
+# full join
+joined_complexes <- reduce(scott_complexes, full_join, by = "complex") %>%
+  column_to_rownames(var = "complex") %>%
+  set_colnames(names(scott_complexes)) %>%
+  as.matrix()
+joined_complexes[is.na(joined_complexes)] <- 0
+
+# save heatmap
+png(file = "heatmap.png", 8000, 6000, pointsize = 12)
+heatmap3(joined_complexes, margins = c(8, 25), na.rm = T)
+dev.off()
