24398ee80fb93cf41b328e9f07b629ab6311db66
CaiCharley <charley.cai113@gmail.com>
2020-07-19 15:45:18 -0700

    add theme to heatmap

--- princeR/detect_complexes/scott_detect_complexes.R
+++ princeR/detect_complexes/scott_detect_complexes.R
@@ -3,6 +3,7 @@ setwd("/mnt/d/Downloads/scottdata")
 
 library(tidyverse)
 library(magrittr)
+library(pals)
 
 # load data as individual list element
 goldstd <- readRDS("goldstd.rds")
@@ -25,16 +26,17 @@ joined_complexes <- reduce(scott_complexes, full_join, by = "complex") %>%
 joined_complexes[is.na(joined_complexes)] <- 0
 
 # filter significant complexes in at least one sample
-significant_complexes <- joined_complexes %>% 
-  as_tibble(rownames = "complex") %>% 
-  rowwise() %>% 
+significant_complexes <- joined_complexes %>%
+  as_tibble(rownames = "complex") %>%
+  rowwise() %>%
   mutate(
     nsig = sum(c_across(where(is.numeric)) >= 1.96),
     bnsig = sum(c_across(contains("bn")) >= 1.96),
     secsig = sum(c_across(contains("sec")) >= 1.96),
-    ) %>% # ! z score
-  filter(bnsig >= 0 && secsig == 3) %>% # ! number of significant replicates
-  select(-nsig, -bnsig, -secsig) %>% 
+  ) %>% # ! z score
+  filter(bnsig >= 3 && secsig == 0) %>% # ! number of significant replicates
+  # filter(nsig >= 3) %>% # ! number of significant replicates
+  select(-nsig, -bnsig, -secsig) %>%
   column_to_rownames(var = "complex")
 
 # hcluster rows and columns
@@ -61,8 +63,36 @@ tidy_complexes %<>%
 
 # make heatmap
 ggplot(tidy_complexes, aes(x = dataset, y = complex, fill = zscore)) +
-  geom_tile() +
-  theme(text = element_text(size = 20))
+  geom_tile(color = "white") +
+  scale_fill_gradientn(colours = gnuplot(25), guide = "colourbar") +
+  scale_x_discrete(
+    expand = c(0, 0),
+    breaks = clevels,
+    labels = str_replace_all(
+      clevels,
+      c(
+        "sec" = "SEC,", "bn" = "BN,", "_" = " ",
+        "unstim" = "M/L", "stim" = "H/L", "1" = " (1)",
+        "2" = " (2)", "3" = " (3)"
+      )
+    )
+  ) +
+  scale_y_discrete(expand = c(0, 0)) +
+  theme(
+    text = element_text(size = 10, family = "TT Arial"),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    panel.border = element_rect(color = "grey50", fill = NA),
+    aspect.ratio = 1,
+    plot.background = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()
+  )
 ggsave(
-  "heatmap_0bn3sec.png",
-  plot = last_plot(), device = "png", width = 30, height = 25)
+  "heatmap_3bn0sec.png",
+  plot = last_plot(), device = "png", unit = "cm", width = 15, height = 10
+)
