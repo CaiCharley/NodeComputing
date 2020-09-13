895f07649f08b5d3ef450dd02704669f7b7088fe
CaiCharley <charley.cai113@gmail.com>
2020-09-12 17:24:16 -0700

    format

--- princeR/compareNB/score_diff.R
+++ princeR/compareNB/score_diff.R
@@ -4,12 +4,12 @@ library(tidyverse)
 library(naivebayes)
 library(PrInCE)
 
-input.Matlab = as.data.frame(read_csv("./all_imputed_feats_bnunstim.csv"))
-input.Matlab$protInCorum[input.Matlab$protInCorum==""] = 0
-input.Matlab$protInCorum[input.Matlab$protInCorum=="\001"] = 1
-input.Matlab$label = rep(NA, nrow(input.Matlab))
-input.Matlab$label[input.Matlab$protInCorum==1 & input.Matlab$intInCorum==0] = 0
-input.Matlab$label[input.Matlab$protInCorum==1 & input.Matlab$intInCorum==1] = 1
+input.Matlab <- as.data.frame(read_csv("./all_imputed_feats_bnunstim.csv"))
+input.Matlab$protInCorum[input.Matlab$protInCorum == ""] <- 0
+input.Matlab$protInCorum[input.Matlab$protInCorum == "\001"] <- 1
+input.Matlab$label <- rep(NA, nrow(input.Matlab))
+input.Matlab$label[input.Matlab$protInCorum == 1 & input.Matlab$intInCorum == 0] <- 0
+input.Matlab$label[input.Matlab$protInCorum == 1 & input.Matlab$intInCorum == 1] <- 1
 
 input.Matlab.labels <- input.Matlab$label
 
@@ -18,7 +18,8 @@ max_euc_quantile <- 0.995
 
 trim_euc <- function(feats) {
   eucs <- feats[c("euc1", "euc2", "euc3")]
-  trimmed <-map_df(eucs,
+  trimmed <- map_df(
+    eucs,
     function(x) {
       threshold <- quantile(x, max_euc_quantile, na.rm = T)
       x[x > threshold] <- threshold
@@ -37,15 +38,15 @@ input <- as_tibble(input.Matlab_Trim) %>%
 labeled <- filter(input, !is.na(label))
 set.seed(1)
 sample <- labeled[sample(1:nrow(labeled), nrow(labeled) * 0.1), ]$id
-Xtr = labeled %>% 
+Xtr <- labeled %>%
   filter(id %in% sample) %>%
-  select(-c(1,2,3,4,5,24,25)) %>%
+  select(-c(1, 2, 3, 4, 5, 24, 25)) %>%
   as.data.frame()
 
 write_csv(Xtr, "Full Xtr.csv")
 
 # training labels, ytr
-ytr = labeled %>% 
+ytr <- labeled %>%
   filter(id %in% sample) %>%
   pull(label)
 
@@ -54,21 +55,21 @@ write_csv(as.data.frame(ytr), "Full Ytr.csv")
 # testing data, Xnew
 Xnew <- filter(input, !(id %in% sample))
 new_ids <- Xnew$id
-Xnew <- select(Xnew, -c(1,2,3,4,5,24,25))
+Xnew <- select(Xnew, -c(1, 2, 3, 4, 5, 24, 25))
 
 write_csv(Xnew, "Full Xnew.csv")
 
 # R precision
-rppis <- predict_ensemble(input.Matlab_Trim[-c(3,4,5)], input.Matlab.labels, "NB", cv_folds = 10, models = 1)
+rppis <- predict_ensemble(input.Matlab_Trim[-c(3, 4, 5)], input.Matlab.labels, "NB", cv_folds = 10, models = 1)
 
 rprecision <- calculate_precision(rppis$label)
 
-ggplot(filter(ppis, n <6000), aes(n, precision)) +
-   geom_path()
+ggplot(filter(ppis, n < 6000), aes(n, precision)) +
+  geom_path()
 
 # calculate classifier score for rows of Xnew
-clf = naive_bayes(Xtr, factor(ytr, levels = c(0,1)))
-score.r = predict(clf, Xnew, type = "prob", threshold = 1e-10)[,2]
+clf <- naive_bayes(Xtr, factor(ytr, levels = c(0, 1)))
+score.r <- predict(clf, Xnew, type = "prob", threshold = 1e-10)[, 2]
 
 # read matlab.score
 score.matlab <- as.data.frame(read_csv("matlab.csv", col_names = F))$X1
