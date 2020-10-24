a8d41d0ad8c83faf955ce8b99bde512278f321e7
CaiCharley <charley.cai113@gmail.com>
2020-10-23 20:08:08 -0700

    move non-CC scripts to PrInCE_Project repo

--- princeR/autocorrelation/wrangle_genekey.R
+++
@@ -1,9 +0,0 @@
-# Make Protein-Gene key
-setwd("D:/Downloads/foster/autocorrelation/")
-
-library(tidyverse)
-
-key <- read_tsv("HUMAN_9606_idmapping.dat", col_names = F) %>%
-  filter(X2 == "Gene_Name") %>%
-  select(1,3) %>%
-  setNames(c("Protein", "Gene"))
